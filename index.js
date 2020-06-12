686294159f904d16f565b7b4febbd297b7ff61dd
Avarokin Saini <saini7@purdue.edu>
2020-06-12 21:13:25 +0530

    Initial ScrollMagic done.

---
+++ index.js
@@ -0,0 +1,31 @@
+const intro = document.querySelector('.intro');
+const video = intro.querySelector('video');
+
+const section2 = document.querySelector('.section-2')
+const end = section2.querySelector('h1');
+
+const controller = new ScrollMagic.Controller();
+
+const scene = new ScrollMagic.Scene({
+  duration : 15040,
+  triggerElement : intro,
+  triggerHook : 0
+})
+.addIndicators()
+.setPin(intro)
+.addTo(controller);
+
+//Animation
+let accel = 0.1;
+let scrollPos = 0;
+let delay = 0;
+
+scene.on('update', e=>{   //Scroll Event Listener
+  scrollPos = e.scrollPos/1000;
+})
+
+
+setInterval(() =>{
+  delay += (scrollPos - delay)*accel;
+  video.currentTime = delay;
+},33.3);
